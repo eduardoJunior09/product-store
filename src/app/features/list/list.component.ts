cdf3ed2f8670614a7169fbf7e49506ed5f1520bf
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-09-26 13:00:12 -0300

    refactor:  refatora listagem de produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,20 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { ProductsService } from './../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [],
-  templateUrl: './list.component.html',
+
+templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  products: any[] = [];
-  httpClient = inject(HttpClient);
+  products: Product[] = [];
+  productsService = inject(ProductsService);
 
   ngOnInit() {
-    this.httpClient.get<any>('/api/products').subscribe((products) => {
-      this.products = products;
-    });
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products; 
+    })
   }
 }
---
+++ src/app/shared/services/products.service.ts
@@ -0,0 +1,14 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../interfaces/product.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  httpClient = inject(HttpClient);
+
+  getAll() {
+    return this.httpClient.get<any>('/api/products');
+  }
+}
